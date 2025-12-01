eedb0f1da80c8d67114756c6d7cafb3e7ab63c66
mohitsharmai1991 <mohitsharma.i1991@gmail.com>
2025-12-01 21:10:43 +0100

    Changes on top of provided repository

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,117 +1,160 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.7.0;
 
-contract Token {
-    string public constant name = "Token";
-    string public constant symbol = "TKN";
-    uint8 public constant decimals = 18;
+import "./IERC20.sol";
+import "./IMintableToken.sol";
+import "./IDividends.sol";
+import "./SafeMath.sol";
 
-    uint256 public totalSupply;
+contract Token is IERC20, IMintableToken, IDividends {
+  // ------------------------------------------ //
+  // ----- BEGIN: DO NOT EDIT THIS SECTION ---- //
+  // ------------------------------------------ //
+  using SafeMath for uint256;
+  uint256 public totalSupply;
+  uint256 public decimals = 18;
+  string public name = "Test token";
+  string public symbol = "TEST";
+  mapping (address => uint256) public balanceOf;
+  // ------------------------------------------ //
+  // ----- END: DO NOT EDIT THIS SECTION ------ //  
+  // ------------------------------------------ //
 
-    mapping(address => uint256) public balanceOf;
-    mapping(address => uint256) public owed;
+  mapping (address => mapping (address => uint256)) public _allowances;
+  
+  // Dividend tracking
+  address[] public tokenHolders;
+  mapping (address => bool) public isTokenHolder;
+  mapping (address => uint256) public withdrawableDividends;
 
-    mapping(address => bool) private known;
-    address[] private holders;
+  // Events
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Mint(address indexed user, uint256 amount);
-    event Burn(address indexed user, uint256 amount);
-    event DividendRecorded(uint256 amount);
-    event DividendWithdrawn(address indexed user, uint256 amount);
+  // IERC20
 
-    function _addHolder(address user) internal {
-        if (!known[user]) {
-            known[user] = true;
-            holders.push(user);
+  function allowance(address owner, address spender) external view override returns (uint256) {
+    return _allowances[owner][spender];
+  }
+
+  function transfer(address to, uint256 value) external override returns (bool) {
+    require(to != address(0), "Transfer to zero address");
+    require(balanceOf[msg.sender] >= value, "Insufficient balance");
+    
+    balanceOf[msg.sender] = balanceOf[msg.sender].sub(value);
+    balanceOf[to] = balanceOf[to].add(value);
+    
+    _addTokenHolder(to);
+    
+    emit Transfer(msg.sender, to, value);
+    return true;
+  }
+
+  function approve(address spender, uint256 value) external override returns (bool) {
+    _allowances[msg.sender][spender] = value;
+    emit Approval(msg.sender, spender, value);
+    return true;
+  }
+
+  function transferFrom(address from, address to, uint256 value) external override returns (bool) {
+    require(to != address(0), "Transfer to zero address");
+    require(balanceOf[from] >= value, "Insufficient balance");
+    require(_allowances[from][msg.sender] >= value, "Insufficient allowance");
+    
+    balanceOf[from] = balanceOf[from].sub(value);
+    balanceOf[to] = balanceOf[to].add(value);
+    _allowances[from][msg.sender] = _allowances[from][msg.sender].sub(value);
+    
+    _addTokenHolder(to);
+    
+    emit Transfer(from, to, value);
+    return true;
+  }
+
+  // IMintableToken
+
+  function mint() external payable override {
+    require(msg.value > 0, "Must send ETH to mint");
+    
+    balanceOf[msg.sender] = balanceOf[msg.sender].add(msg.value);
+    totalSupply = totalSupply.add(msg.value);
+    
+    _addTokenHolder(msg.sender);
+    
+    emit Transfer(address(0), msg.sender, msg.value);
+  }
+
+  function burn(address payable dest) external override {
+    uint256 amount = balanceOf[msg.sender];
+    require(amount > 0, "No tokens to burn");
+    
+    balanceOf[msg.sender] = 0;
+    totalSupply = totalSupply.sub(amount);
+    
+    emit Transfer(msg.sender, address(0), amount);
+    
+    dest.transfer(amount);
+  }
+
+  // IDividends
+
+  function getNumTokenHolders() external view override returns (uint256) {
+    uint256 count = 0;
+    for (uint256 i = 0; i < tokenHolders.length; i++) {
+      if (balanceOf[tokenHolders[i]] > 0) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  function getTokenHolder(uint256 index) external view override returns (address) {
+    require(index > 0, "Index must be greater than 0");
+    
+    uint256 count = 0;
+    for (uint256 i = 0; i < tokenHolders.length; i++) {
+      if (balanceOf[tokenHolders[i]] > 0) {
+        count++;
+        if (count == index) {
+          return tokenHolders[i];
         }
+      }
     }
+    
+    revert("Index out of bounds");
+  }
 
-    function _removeHolder(address user) internal {
-        if (known[user] && balanceOf[user] == 0) {
-            known[user] = false;
-            uint256 len = holders.length;
-            for (uint256 i = 0; i < len; i++) {
-                if (holders[i] == user) {
-                    holders[i] = holders[len - 1];
-                    holders.pop();
-                    break;
-                }
-            }
-        }
+  function recordDividend() external payable override {
+    require(msg.value > 0, "Must send ETH for dividend");
+    require(totalSupply > 0, "No tokens in circulation");
+    
+    for (uint256 i = 0; i < tokenHolders.length; i++) {
+      address holder = tokenHolders[i];
+      uint256 holderBalance = balanceOf[holder];
+      
+      if (holderBalance > 0) {
+        uint256 dividend = msg.value.mul(holderBalance).div(totalSupply);
+        withdrawableDividends[holder] = withdrawableDividends[holder].add(dividend);
+      }
     }
+  }
 
-    function transfer(address to, uint256 amount) external returns (bool) {
-        require(balanceOf[msg.sender] >= amount, "insufficient");
+  function getWithdrawableDividend(address payee) external view override returns (uint256) {
+    return withdrawableDividends[payee];
+  }
 
-        balanceOf[msg.sender] -= amount;
-        balanceOf[to] += amount;
+  function withdrawDividend(address payable dest) external override {
+    uint256 amount = withdrawableDividends[msg.sender];
+    require(amount > 0, "No dividends to withdraw");
+    
+    withdrawableDividends[msg.sender] = 0;
+    dest.transfer(amount);
+  }
 
-        _addHolder(to);
-        _removeHolder(msg.sender);
-
-        emit Transfer(msg.sender, to, amount);
-        return true;
+  // Internal helper function
+  function _addTokenHolder(address holder) internal {
+    if (!isTokenHolder[holder] && holder != address(0)) {
+      tokenHolders.push(holder);
+      isTokenHolder[holder] = true;
     }
-
-    function mint() external payable {
-        require(msg.value > 0, "no eth");
-
-        balanceOf[msg.sender] += msg.value;
-        totalSupply += msg.value;
-
-        _addHolder(msg.sender);
-
-        emit Mint(msg.sender, msg.value);
-        emit Transfer(address(0), msg.sender, msg.value);
-    }
-
-    function burn(uint256 amount) external {
-        require(balanceOf[msg.sender] >= amount, "insufficient");
-
-        balanceOf[msg.sender] -= amount;
-        totalSupply -= amount;
-
-        _removeHolder(msg.sender);
-
-        (bool ok, ) = msg.sender.call{value: amount}("");
-        require(ok, "eth fail");
-
-        emit Burn(msg.sender, amount);
-        emit Transfer(msg.sender, address(0), amount);
-    }
-
-    function recordDividend() external payable {
-        require(msg.value > 0, "empty");
-
-        uint256 n = holders.length;
-        if (n == 0) return;
-
-        for (uint256 i = 0; i < n; i++) {
-            address user = holders[i];
-            uint256 bal = balanceOf[user];
-            if (bal == 0) continue;
-
-            uint256 share = (msg.value * bal) / totalSupply;
-            owed[user] += share;
-        }
-
-        emit DividendRecorded(msg.value);
-    }
-
-    function withdrawDividends() external {
-        uint256 amt = owed[msg.sender];
-        require(amt > 0, "none");
-
-        owed[msg.sender] = 0;
-
-        (bool ok, ) = msg.sender.call{value: amt}("");
-        require(ok, "fail");
-
-        emit DividendWithdrawn(msg.sender, amt);
-    }
-
-    function holdersLength() external view returns (uint256) {
-        return holders.length;
-    }
+  }
 }
